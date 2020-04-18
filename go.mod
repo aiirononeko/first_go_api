f663d3370d0a6909be0ad3e7d04d2a19a25f519b
片田凌太 <aiirononeko@gmail.com>
2020-04-19 01:04:21 +0900

    変更

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/aiirononeko/first_go_api
 go 1.14
 
 require (
+	github.com/jinzhu/gorm v1.9.12 // indirect
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	golang.org/x/crypto v0.0.0-20200414173820-0848c9571904 // indirect
--- main.go
+++ main.go
@@ -1,21 +1,92 @@
+// package main
+
+// import (
+// 	"net/http"
+// 	"github.com/labstack/echo"
+// 	"os"
+// 	"github.com/jinzhu/gorm"
+//   _ "github.com/jinzhu/gorm/dialects/sqlite"
+// )
+
+// type User struct {
+// 	Email    string
+// 	Password string
+// }
+
+// func main() {
+// 	// サーバー用のインスタンスの取得
+// 	e := echo.New()
+
+// 	// DBConnection
+// 	db, err := gorm.Open("sqlite3", "test.db")
+//   if err != nil {
+//     panic("データベースへの接続に失敗しました")
+//   }
+// 	defer db.Close()
+
+// 	// migration
+// 	db.AutoMigrate(&User{})
+
+// 	// ルーティング設定
+// 	e.GET("/helloworld", helloWorld)
+// 	e.POST("/create", func(c echo.Context) error {
+// 		return db.Create(&User{Email: "me@example.com", Password: "password"})
+// 	})
+
+// 	// サーバー起動
+// 	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
+// }
+
+// func createUser(c echo.Context) error {
+// 	return db.Create(&User{Email: "me@example.com", Password: "password"})
+// }
+
+// func helloWorld(c echo.Context) error {
+// 	return c.String(http.StatusOK, "hello world!!")
+// }
+
 package main
+
 import (
 	"net/http"
-	"github.com/labstack/echo"
 	"os"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/labstack/echo"
 )
+
 type User struct {
+	gorm.Model
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
+
+var (
+	db *gorm.DB
+)
+
 func main() {
+	// gormでmysqlかsqliteに接続
+	var err error
+	db, err = gorm.Open("sqlite3", "/tmp/gorm.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	// スキーマのマイグレーション
+	db.AutoMigrate(&User{})
+
 	// サーバー用のインスタンスの取得
 	e := echo.New()
+
 	// ユーザー
 	u := User{
 		Email:    "me@example.com",
 		Password: "password",
 	}
+
 	// ルーティング設定
 	e.GET("/helloworld", helloWorld)
 	e.POST("/login", func(c echo.Context) error {
@@ -28,11 +99,25 @@ func main() {
 		}
 		// 暫定
 		token := "sampletoken"
+		// gormでメアドとパスワードをDBに保存
+		db.Create(&User{Email: "me@example.com", Password: "password"})
+
 		return c.String(http.StatusOK, "{\"token\":\""+token+"\"}")
 	})
+	// 過去ログインしてきた履歴を返す
+	e.GET("/loginhistory", loginhistory)
+
 	// サーバー起動
 	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
 }
+
 func helloWorld(c echo.Context) error {
 	return c.String(http.StatusOK, "hello world!!")
+}
+
+func loginhistory(c echo.Context) error {
+	// gormでメアドとパスワードを保存したテーブルから、データ取得
+	var user User
+	db.Find(&user)
+	return c.JSON(http.StatusOK, user)
 }
